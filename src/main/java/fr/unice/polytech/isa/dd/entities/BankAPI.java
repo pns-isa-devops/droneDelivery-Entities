3eed950670f9a53f44ab7fb697894734781b4927
PaulK <merveille.koffi@etu.unice.fr>
2020-05-10 21:15:54 +0200

    #68 fix bug

--- src/main/java/fr/unice/polytech/isa/dd/entities/BankAPI.java
+++ src/main/java/fr/unice/polytech/isa/dd/entities/BankAPI.java
@@ -6,6 +6,11 @@ import org.apache.cxf.jaxrs.client.WebClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.SQLOutput;
 
 public class BankAPI {
@@ -40,27 +45,119 @@ public class BankAPI {
         return payment;
     }
 
+//    public JSONArray getPayements() throws ExternalPartnerException {
+//
+//        JSONArray res = null;
+//        try {
+//            System.out.println("-------------------------- Ping  external Service /  Payment ------------------------------------");
+//            System.out.println();
+//            String response = WebClient.create("http://192.168.99.100:9090").path("/payments").get(String.class);
+//            res = new JSONArray(response);
+//            System.out.println("Fuck ");
+//        } catch (Exception e) {
+//            // ### ou soit essayer de pinguer le serveur jenkins
+//            System.out.println("-------------------------- Ping jenkins external Service /  Payment ------------------------------------");
+//            try {
+//                String response = WebClient.create("http://jenkins-teamd.francecentral.cloudapp.azure.com:9090/").path("/payments").get(String.class);
+//                res = new JSONArray(response);
+//            } catch (Exception de) {
+//                throw new ExternalPartnerException(url + "/payments", de);
+//            }
+//            return res;
+//        }
+//        //return (payment.getInt("Status") == 0);
+//        return res;
+//
+//        try {
+////            URL url = new URL("http://192.168.99.100:9090/payments");
+//            //            URL url = new URL("http://localhost:9090/payments");
+//
+//            URL url = new URL("http://jenkins-teamd.francecentral.cloudapp.azure.com:9090/payments");
+//
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//            connection.connect();
+//            int respCode = connection.getResponseCode();
+//            System.out.println("=====> Jenkins Server Extern UP");
+//            System.out.println(respCode);
+//            System.out.println("Resp " + connection.getContent());
+//            System.out.println();
+//
+//            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//            StringBuilder sb = new StringBuilder();
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                sb.append(line+"\n");
+//            }
+//            br.close();
+//            System.out.println("Resp "+sb.toString());
+//        } catch (IOException e) {
+//            System.out.println("=====> Jenkins Server Extern DOWN");
+//            System.out.println();
+//
+//        }
+
+
     public JSONArray getPayements() throws ExternalPartnerException {
 
         JSONArray res = null;
+//        try {
+//            System.out.println("-------------------------- Ping  external Service /  Payment ------------------------------------");
+//            System.out.println();
+//            String response = WebClient.create("http://192.168.99.100:9090").path("/payments").get(String.class);
+//            res = new JSONArray(response);
+//            System.out.println("Fuck ");
+//        } catch (Exception e) {
+//            // ### ou soit essayer de pinguer le serveur jenkins
+//            System.out.println("-------------------------- Ping jenkins external Service /  Payment ------------------------------------");
+//            try {
+//                String response = WebClient.create("http://jenkins-teamd.francecentral.cloudapp.azure.com:9090/").path("/payments").get(String.class);
+//                res = new JSONArray(response);
+//            } catch (Exception de) {
+//                throw new ExternalPartnerException(url + "/payments", de);
+//            }
+//            return res;
+//        }
+        //return (payment.getInt("Status") == 0);
+
         try {
-            System.out.println("-------------------------- Ping  external Service /  Payment ------------------------------------");
-            System.out.println();
-            String response = WebClient.create(url).path("/payments").get(String.class);
-            res = new JSONArray(response);
-        } catch (Exception e) {
-            // ### ou soit essayer de pinguer le serveur jenkins
-            System.out.println("-------------------------- Ping jenkins external Service /  Payment ------------------------------------");
+            URL url = new URL("http://jenkins-teamd.francecentral.cloudapp.azure.com:9090/payments");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int respCode = connection.getResponseCode();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+            res = new JSONArray(sb.toString());
+        } catch (IOException e) {
             try {
-                String response = WebClient.create("http://jenkins-teamd.francecentral.cloudapp.azure.com:9090/").path("/payments").get(String.class);
-                res = new JSONArray(response);
-            } catch (Exception de) {
+                System.out.println("Second option");
+//                URL url = new URL("http://192.168.99.100:9090/payments");
+                URL url = new URL("http://localhost:9090/payments");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                int respCode = connection.getResponseCode();
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                br.close();
+                res = new JSONArray(sb.toString());
+            } catch (IOException de) {
                 throw new ExternalPartnerException(url + "/payments", de);
             }
-            return res;
+
         }
-        //return (payment.getInt("Status") == 0);
         return res;
+
     }
 
 }
